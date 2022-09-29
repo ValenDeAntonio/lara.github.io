09f8e136b25ce1c7051793f3ba8d7e54be672a5a
Valentín De Antonio <77996041+ValenDeAntonio@users.noreply.github.com>
2022-09-29 12:32:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var myBlurFunction = function(state) {
+  /* state can be 1 or 0 */
+  var containerElement = document.getElementById('main_container');
+  var overlayEle = document.getElementById('overlay');
+
+  if (state) {
+    overlayEle.style.display = 'block';
+    containerElement.setAttribute('class', 'blur');
+  } else {
+    overlayEle.style.display = 'none';
+    containerElement.setAttribute('class', null);
+  }
+};
+
+document.getElementById('invisible').onclick = function() {
+  var containerElement = document.getElementById('main_container');
+  var overlayEle = document.getElementById('overlay');
+  overlayEle.style.display = 'none';
+  containerElement.setAttribute('class', null);
+}
